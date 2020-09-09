27ea259f19d20062b4d7f983da2c8a7a38d78690
Adam Krizar <apocalypsearisen44@gmail.com>
2020-09-10 00:04:16 +0200

    AdvancedRedAllert-Script
    Reeee

--- AdvancedRedAllert-Script/Program.cs
+++ AdvancedRedAllert-Script/Program.cs
@@ -21,62 +21,71 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
+
+        private string NORMAL_ALLERT = "STATUS: NORMAL";
+        private string YELLOW_ALLERT = "STATUS: YELLOW";
+        private string RED_ALLERT = "RED ALLERT";
+
+        private int ALLERT_STATE = 0;
+
+        private IMyProgrammableBlock EnergyControl;
+
+        private List<IMyTextPanel> infoScreens;
 
         public Program()
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set Runtime.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
+            List<IMyTextPanel> temp = new List<IMyTextPanel>();
+            infoScreens = new List<IMyTextPanel>();
+            GridTerminalSystem.GetBlocksOfType(infoScreens);
+            foreach(IMyTextPanel panel in infoScreens)
+            {
+                if (IsOnThisGrid(panel) && panel.CustomName.Contains("INFO_SCREEN"))
+                {
+                    temp.Add(panel);
+                }
+            }
+
+            infoScreens = temp;
         }
 
-        public void Save()
+        private bool IsOnThisGrid(IMyCubeBlock block)
         {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
+            if (Me.CubeGrid.Equals(block.CubeGrid)) return true;
+            else return false;
+        }
+
+        private void NormalStatus()
+        {
+            foreach(IMyTextPanel panel in infoScreens)
+            {
+                panel.WriteText(NORMAL_ALLERT);
+            }
+        }
+
+        private void YellowStatus()
+        {
+            foreach (IMyTextPanel panel in infoScreens)
+            {
+                panel.WriteText(YELLOW_ALLERT);
+            }
+        }
+
+        private void RedStatus()
+        {
+            foreach (IMyTextPanel panel in infoScreens)
+            {
+                panel.WriteText(RED_ALLERT);
+            }
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
+            switch(argument)
+            {
+                case "0": NormalStatus(); break;
+                case "1": YellowStatus(); break;
+                case "2": RedStatus(); break;
+            }
         }
     }
 }
