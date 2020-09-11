ecea4bf83dc4f0fbe1d8958314f0408f2c9c0264
Adam Krizar <apocalypsearisen44@gmail.com>
2020-09-11 22:29:01 +0200

    added Colors

--- AdvancedRedAllert-Script/Program.cs
+++ AdvancedRedAllert-Script/Program.cs
@@ -21,16 +21,18 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-
         private string NORMAL_ALLERT = "STATUS: NORMAL";
         private string YELLOW_ALLERT = "STATUS: YELLOW";
-        private string RED_ALLERT = "RED ALLERT";
-
-        private int ALLERT_STATE = 0;
+        private string RED_ALLERT = ">  RED ALERT <";
 
         private IMyProgrammableBlock EnergyControl;
 
         private List<IMyTextPanel> infoScreens;
+
+        private List<IMyLightingBlock> primaryLights;
+        private List<IMyLightingBlock> mudLights;
+
+        private bool ON_ALERT = false;
 
         public Program()
         {
@@ -44,8 +46,27 @@ namespace IngameScript
                     temp.Add(panel);
                 }
             }
-
             infoScreens = temp;
+
+            List<IMyLightingBlock> temp2 = new List<IMyLightingBlock>();
+            primaryLights = new List<IMyLightingBlock>();
+            mudLights = new List<IMyLightingBlock>();
+            GridTerminalSystem.GetBlocksOfType(temp2);
+            foreach(IMyLightingBlock light in temp2)
+            {
+                if(IsOnThisGrid(light))
+                {
+                    if (light.CustomName.Contains("NORMAL_LIGHTS"))
+                    {
+                        primaryLights.Add(light);
+                    }
+                    else if (light.CustomName.Contains("MUD_LIGHTS"))
+                    {
+                        mudLights.Add(light);
+                    }
+                }
+            }
+
         }
 
         private bool IsOnThisGrid(IMyCubeBlock block)
@@ -56,25 +77,88 @@ namespace IngameScript
 
         private void NormalStatus()
         {
-            foreach(IMyTextPanel panel in infoScreens)
-            {
-                panel.WriteText(NORMAL_ALLERT);
+            Runtime.UpdateFrequency = UpdateFrequency.None;
+            //Text
+            foreach (IMyTextPanel panel in infoScreens)
+            {                
+                panel.BackgroundColor = Color.Black;
+                panel.FontColor = Color.Green;
+                panel.WriteText(NORMAL_ALLERT);                
             }
+            //Lights
+            foreach (IMyLightingBlock light in primaryLights)
+            {
+                light.Intensity = 10;
+            }
+            foreach (IMyLightingBlock light in mudLights)
+            {
+                light.Color = Color.White;
+                light.BlinkIntervalSeconds = 0;
+            }
+
+            ON_ALERT = false;
         }
 
         private void YellowStatus()
         {
+            Runtime.UpdateFrequency = UpdateFrequency.None;
+            //Text
             foreach (IMyTextPanel panel in infoScreens)
             {
+                //Text
+                panel.BackgroundColor = Color.Black;
+                panel.FontColor = Color.Yellow;
                 panel.WriteText(YELLOW_ALLERT);
             }
+            //Lights
+            foreach (IMyLightingBlock light in primaryLights)
+            {
+                light.Intensity = 10;
+            }
+            foreach (IMyLightingBlock light in mudLights)
+            {
+                light.Color = Color.Yellow;
+                light.BlinkIntervalSeconds = 2;
+                light.BlinkLength = 50;
+            }
+
+            ON_ALERT = false;
         }
 
         private void RedStatus()
         {
+            if (!ON_ALERT)
+            {
+                //Text
+                foreach (IMyTextPanel panel in infoScreens)
+                {
+                    panel.BackgroundColor = Color.Red;
+                    panel.FontColor = Color.Black;
+                    panel.WriteText(">  " + RED_ALLERT + "  <");
+                }
+                //Lights
+                foreach (IMyLightingBlock light in primaryLights)
+                {
+                    light.Intensity = 4;
+                }
+                foreach (IMyLightingBlock light in mudLights)
+                {
+                    light.Color = Color.Red;
+                    light.BlinkIntervalSeconds = 2;
+                    light.BlinkLength = 50;
+                }
+
+                ON_ALERT = true;
+                Runtime.UpdateFrequency = UpdateFrequency.Update10;
+            }
+            else Animation();
+        }
+
+        private void Animation()
+        {
             foreach (IMyTextPanel panel in infoScreens)
             {
-                panel.WriteText(RED_ALLERT);
+                panel.WriteText(" > " + RED_ALLERT + " < ");
             }
         }
 
@@ -85,6 +169,7 @@ namespace IngameScript
                 case "0": NormalStatus(); break;
                 case "1": YellowStatus(); break;
                 case "2": RedStatus(); break;
+                default: NormalStatus(); break;
             }
         }
     }
